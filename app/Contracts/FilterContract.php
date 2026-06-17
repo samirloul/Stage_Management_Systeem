079f1027d1dc3158ddc02e2580a28a988a74cc18
samirloul <sameerloul2010@gmail.com>
2026-06-17 11:40:12 +0200

    Voeg docblock toe aan FilterContract interface: polymorfisme en Strategy pattern toegelicht

---
+++ app/Contracts/FilterContract.php
@@ -0,0 +1,17 @@
+﻿<?php
+
+namespace App\Contracts;
+
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * Contract (interface) voor alle filterklassen in de applicatie.
+ *
+ * Door alle filters aan deze interface te koppelen (polymorfisme),
+ * kunnen StudentFilter, CompanyFilter en InternshipFilter uitwisselbaar worden gebruikt.
+ * Dit is een voorbeeld van het Strategy design pattern.
+ */
+interface FilterContract
+{
+    public function apply(Builder $query, array $filters): Builder;
+}
