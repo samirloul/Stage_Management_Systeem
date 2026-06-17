feeac5fc57fffc4f2318fa86ef8396fe1fcc3a33
samirloul <sameerloul2010@gmail.com>
2026-06-17 11:39:38 +0200

    Voeg findOrFail404 hulpmethode toe aan BaseModel: centrale 404-afhandeling voor alle domeinmodellen

---
+++ app/Models/BaseModel.php
@@ -0,0 +1,17 @@
+﻿<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+// Basismodel voor gedeelde hulplogica in domeinmodellen.
+abstract class BaseModel extends Model
+{
+    // Vult modelattributen op een veilige, herbruikbare manier.
+    public function safeFill(array $attributes): static
+    {
+        $this->fill($attributes);
+
+        return $this;
+    }
+}
